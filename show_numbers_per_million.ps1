71dbaa1619c04c5147ec7aa612418915d6b1da42
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2022-04-28 12:30:47 +0200

    Update show_numbers_per_million.ps1

--- show_numbers_per_million.ps1
+++ show_numbers_per_million.ps1
@@ -1,11 +1,11 @@
 $url = "https://github.com/owid/covid-19-data/raw/master/public/data/owid-covid-data.csv"
-$datei = "daten.csv"
+$file = "daten.csv"
 
-if ( -not (Test-Path $datei) -or ((Get-Item $datei).CreationTime -lt (Get-Date).AddDays(-1)) ) {
-    Invoke-WebRequest $url -OutFile $datei    
+if ( -not (Test-Path $file) -or ((Get-Item $file).CreationTime -lt (Get-Date).AddDays(-1)) ) {
+    Invoke-WebRequest $url -OutFile $file    
 }
 
-$csv = Import-Csv -Delimiter "," $datei
+$csv = Import-Csv -Delimiter "," $file
 
 $groups_by_countries = $csv | Group-Object location -AsHashTable
 
@@ -14,4 +14,11 @@ $countries = $groups_by_countries.Keys
 $countries | ForEach-Object {
     $groups_by_countries."$_" | Format-Table date,@{l="new cases per million";e={[float]$_.new_cases_per_million}},@{l="new deaths per million";e={[float]$_.new_deaths_per_million}}
 }
+
+
+
+
+
+
+
 
